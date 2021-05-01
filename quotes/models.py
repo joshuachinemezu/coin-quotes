e19628daba3a317550a9ca90cfd7203adb29a828
Joshua Chinemezu <joshuachinemezu@gmail.com>
2021-05-01 07:08:58 +0400

    Add models and update migrations

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Quote(models.Model):
+    from_currency_code = models.CharField(max_length=100)
+    from_currency_name = models.CharField(max_length=100)
+    to_currency_code = models.CharField(max_length=100)
+    to_currency_name = models.CharField(max_length=100)
+    exchange_rate = models.CharField(max_length=100)
+    last_refreshed = models.CharField(max_length=100)
+    time_zone = models.CharField(max_length=100)
+    bid_price = models.CharField(max_length=100)
+    ask_price = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = True
+        db_table = 'quotes'
